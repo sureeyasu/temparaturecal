810cb547b2fe552ca7f9bcd7a85c5845ee2fb239
unknown <suree.yakkala@gmail.com>
2021-01-20 00:47:30 -0800

    remove the  sample

--- src/main/java/com/sparity/temparature/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparity.temparature.repository;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-import com.sparity.temparature.model.Company;
-
-public interface CompanyRepository extends Neo4jRepository<Company,Long>{
-
-}
